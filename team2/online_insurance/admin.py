0b636608e1af908080488128f25f13f679de0582
Syed Sharukhali Farookali Naziyabegum <shahsai11111@gmail.com>
2024-05-19 19:58:52 +0530

    .

--- team2/online_insurance/admin.py
+++ team2/online_insurance/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import AgentAvailability,Appointment,Policy
-# Register your models here.
+# Register your models here
 
 admin.site.register(AgentAvailability)
 admin.site.register(Appointment)
